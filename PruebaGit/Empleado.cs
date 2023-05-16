2bde8ef2f4a96faee6cee673d0f79be09a6fb046
Emiliano R <emiliano.r.javier@gmail.com>
2023-05-16 13:23:36 -0300

    Se crea clase empleado

---
+++ PruebaGit/Empleado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaGit
+{
+    public class Empleado : PersonaEmiliano
+    {
+        public string legajo {get; set;}
+
+        public DateTime entrada = DateTime.Now;
+
+    }
+
+}
